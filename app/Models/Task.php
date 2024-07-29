725ddb41d628dc7d316a12fbf04f3ed5bb87b846
Oluwatos94 <akinbowat@gmail.com>
2024-07-29 23:23:39 +0100

    Tasks model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,4 +8,37 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'status',
+        'priority',
+        'assigned_user_id',
+        'project_id',
+        'image_path',
+        'due_date',
+        'created_by',
+        'updated_by',
+    ];
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
+
+    public function assignedUser()
+    {
+        return $this->belongsTo(User::class, 'assigned_user_id');
+    }
+
+    public function createdBy()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function updatedBy()
+    {
+        return $this->belongsTo(User::class, 'updated_by');
+    }
 }
